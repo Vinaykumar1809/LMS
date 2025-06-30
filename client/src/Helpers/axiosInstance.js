6c39f838e0d16210dc18632e1f53ec77eebffd78
Vinaykumar1809 <vinaykchaudhary2006@gmail.com>
2025-06-30 07:59:14 +0530

    Update axiosInstance.js
    removed the development testing console logs

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-console.log("Backend URL: ", import.meta.env.VITE_BACKEND_URL);
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
